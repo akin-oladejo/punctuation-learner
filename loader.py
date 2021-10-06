2aae83f750b98752afb45019570433b22d3097c8
Damilare Akin-Oladejo <akinoladejodamilare@gmail.com>
2021-10-07 00:25:46 +0100

    feat: require two files for training

--- loader.py
+++ loader.py
@@ -13,13 +13,15 @@ class Loader:
         self.verify_dataset(self._path) # verify dataset and populate training list
         self._puncs = self.extract_punc() # extract the punctuation in each book
         self._data = self.conv_punc(self._puncs) # convert the punctuation to numerical embeddings
-        print_dict(self._data)
+        print(self._training_files)
+        # print_dict(self._data)
         # self._train, self._test = self.split(self._data) # split data into train and test
 
         
     def verify_dataset(self, path_url):
-        '''logic to verify that the dataset exists, contains sub-authors, warn if there are stray files/folders
-        and make sure that the sub folders (which should house an author's books) contain .txt files'''
+        '''logic to verify that the dataset exists, contains sub-authors, warn if there are stray files/folders,
+        make sure that the sub folders (which should house an author's books) contain .txt files
+        and there are at least two .txt files for each other so test-train can occur'''
 
         if path.exists(path_url): # check that path exists
             if path.isdir(path_url): # check that path is a folder
@@ -31,38 +33,51 @@ class Loader:
                 
                 for l1_sub in l1: 
                     sub_path = path_url + '/' + l1_sub # assign location of sub to a variable
-                    if path.isfile(sub_path): # if parent folder contains stray files i.e files not in label folder
-                        warn(f"'{sub_path}' is not in a label folder; it will not be used in training", typ='file')
+                    if path.isfile(sub_path): # if parent folder contains stray files i.e files not in author folder
+                        warn(f"'{sub_path}' is not in an author folder; it will not be used in training", typ='file')
 
                     elif path.isdir(sub_path): # what to do when the child is a directory; this is the desired case
+                        txt_count = 0 # counter for the number of txt files found in author folder
+
                         if not self._training_files.get(l1_sub): self._training_files[l1_sub] = []
                         l2 = os.listdir(sub_path) # l2 means grandchild of dataset path
-                        
-                        # alert that a child folder is empty if it does not contain children
-                        if len(l2)<1:
-                            warn(f"'{l1_sub}' folder is empty; it will not be used in training", typ='special')
 
                         for l2_sub in l2:
                             sub_path2 = sub_path + '/' + l2_sub # assign location of grandchild to a variable
-                            if path.isdir(sub_path2): #if a dir is found inside a label, ignore it
-                                warn(f"'{sub_path2}' is another folder inside a label folder; it will be not be used in training", typ='folder')
+                            if path.isdir(sub_path2): #if a folder is found inside an author folder, ignore it
+                                warn(f"'{sub_path2}' is another folder inside an author folder; it will be not be used in training", typ='folder')
                             
                             # what to do if grandchild is a file
                             elif path.isfile(sub_path2):
 
+                                # if a granchild is not a .txt file, ignore
+                                if not l2_sub.endswith('.txt'): 
+                                    warn(f"'{sub_path2}' file is not a .txt file; it will not be used in training", typ='special')
+
                                 # what to do if grandchild is a text file; this is the desired case
-                                if l2_sub.endswith('.txt'): 
+                                elif l2_sub.endswith('.txt'): 
                                     with open(sub_path2) as f:
                                         if len(f.readlines()) < 100:
-                                            warn(f"'{l2_sub}' does not contain enough text to train; it will not be used in training", typ='file')
+                                            warn(f"'{sub_path2}' does not contain enough text to train; it will not be used in training", typ='file')
                                         
                                         # alas, we have gotten to a file good enough? to train with
-                                        else: self._training_files[l1_sub].append(sub_path2) # add file path to author list
-                                elif not l2_sub.endswith('.txt'): # if a granchild is not a .txt file
-                                    warn(f"'{sub_path2}' file is not a .txt file; it will not be used in training", typ='special')
+                                        else: 
+                                            txt_count += 1 # increase the count of .txt files for that author
+                                            self._training_files[l1_sub].append(sub_path2) # add file path to author list
+                                
+                        
+                        # for train-test purposes, at least two books are needed for each author
+                        if txt_count < 2:
+                            self._training_files.pop(l1_sub)
+                            warn(f"At least two works by '{l1_sub}' are needed to train, {txt_count} found. 'Data! We need more data!'", typ='special')
+                        # print(l1_sub, 'Number of txt files found',txt_count)
+                        
+                if len(self._training_files)==0: # if no .txt files found at all
+                    warn("No author found in directory after search!")
                 
-                if len(self._training_files)<1: # if no .txt files found at all
-                    warn("No label or data found in directory after search! 'Data! We need more data!'")
+                # # for train-test purposes, at least two books are needed for each author
+                # if txt_count < 2:
+                #     warn('At least two valid .txt files are needed to train!')
             else: warn("Path is not a directory! :(")
         else: warn("Path does not exist! Stap playin' wimme")
 
